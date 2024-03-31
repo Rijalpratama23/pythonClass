8a1ac99ff87472fb738de8fceded0b4056b5e2ab
RijalPratama23 <160234158+Rijalpratama23@users.noreply.github.com>
2024-03-31 13:17:06 +0700

    Update kembalian.py

--- Pertemuan1/kembalian.py
+++ Pertemuan1/kembalian.py
@@ -1,5 +1,5 @@
 uangSaya = int(input("masukan total uang : "))
 uangDigunakan = 15000
-uangKembalian = uangSaya - uangDi20gunakan
+uangKembalian = uangSaya - uangDigunakan
 
 print("uangKembalian %i uang, uangSaya %i"%(uangKembalian, uangSaya))
